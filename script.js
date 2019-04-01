3258be1262eea770e19db8233c82e51cca74a250
Glitch (hello-webpage) <none>
2019-04-01 03:09:09 +0000

    📹🎉 Checkpoint ./script.js:729508/225 ./style.css:729508/105

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var tags = ["words", "art", "sounds", "cats", "nothing"];
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
