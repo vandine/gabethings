2cffbed8d10a8975a90ff255053c0a7f6271befe
Glitch (hello-webpage) <none>
2019-04-01 05:18:10 +0000

    😽🚙 Checkpoint ./script.js:729508/774

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //dummy tags
-var tags = ["words", "art", "sounds", "cats", "nothing"];
+var pages = [["words", "Test Words", "a test words post...", "https://imgur.com/"], ["pictures", "Test Pictures", "a test pictures post...", "https://imgur.com/"],
+             ["sounds", "Test Sounds", "a test sounds post...", "https://imgur.com/"]];
 
 function search(){
+  
+  searchVal = $(".pagelist").val();
+  
+  for(var i=0; i < pages.length();i++){
+    if(pages[i][0].includes(searchVal){
+       makePost();
+    }
+    if else{
+      return 0;
+    }
+  
 }
+
+function makePost(tag, name, blurb){
+  htmlTag = 
