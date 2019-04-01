292a780735c4879605622a16731e3b943aecd7d9
Glitch (hello-webpage) <none>
2019-04-01 06:02:10 +0000

    🏫💆 Checkpoint ./script.js:729508/15637 ./index.html:729508/93

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //dummy tags
-var pages = [["words", "Test Words", "a test words post...", "https://imgur.com/"], ["pictures", "Test Pictures", "a test pictures post...", "https://imgur.com/"],
-             ["sounds", "Test Sounds", "a test sounds post...", "https://imgur.com/"]];
+var pages = [["words", "Test Words", "a test words post...", "https://imgur.com/", "https://pbs.twimg.com/profile_images/834978723149447172/-pe9YWrw_400x400.jpg"], 
+             ["pictures", "Test Pictures", "a test pictures post...", "https://imgur.com/", "https://pbs.twimg.com/profile_images/834978723149447172/-pe9YWrw_400x400.jpg"],
+             ["sounds", "Test Sounds", "a test sounds post...", "https://imgur.com/", "https://pbs.twimg.com/profile_images/834978723149447172/-pe9YWrw_400x400.jpg"]];
 
 function search(){
   
   var searchVal = $(".pagelist").val();
   
-  for(var i=0; i < pages.length();i++){
-    console.log(pages[i][0]);
+  for(var i=0; i < pages.length; i++){
     if(pages[i][0].includes(searchVal)){
        $(".results").append(makeCard(pages[i][1], pages[i][2]));
-      alert("appended " + pages[i][0] + "to search results.");
     }
     else{
       return 0;
@@ -19,6 +18,8 @@ function search(){
 }
 }
 
+
+//generate cards for search testing
 function makeCard(name, blurb){
   return "<li class='proj-card'>"+makeName(name)+makeBlurb(blurb)+"</li>"
 }
@@ -34,14 +35,10 @@ function makeBlurb(blurb){
   return htmlBlurb;
 }
 
-function test(){
-  var x = $(".pagelist").val();
-  x.value = x.value.toUpperCase();
-}
+function makePicLink(link){
+  var htmlLink = "<a href='"+link+"><img src="https://pbs.twimg.com/profile_images/834978723149447172/-pe9YWrw_400x400.jpg" style="width:100%;" class="proj-img">
+        </a>
 
-$(".dumb").click(function(){
-    alert(pages[0][1]);
-}
   
 
 
