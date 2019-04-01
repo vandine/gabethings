8d69d2a2e8fa2dc4a63c99e7af3c8fb7f2cf4a6c
Glitch (hello-webpage) <none>
2019-04-01 05:29:12 +0000

    🎒⚽️ Checkpoint ./script.js:729508/828

--- script.js
+++ script.js
@@ -4,17 +4,34 @@ var pages = [["words", "Test Words", "a test words post...", "https://imgur.com/
 
 function search(){
   
-  searchVal = $(".pagelist").val();
+  var searchVal = $(".pagelist").val();
   
   for(var i=0; i < pages.length();i++){
-    if(pages[i][0].includes(searchVal){
-       makePost();
+    if(pages[i][0].includes(searchVal)){
+       $(".results").append(makeCard(pages[i][1], pages[i][2]));
+      alert("appended " + pages[i][0] + "to search results.");
     }
-    if else{
+    else{
       return 0;
     }
   
 }
+}
 
-function makePost(tag, name, blurb){
-  htmlTag = 
+function makeCard(name, blurb){
+  return "<li class='proj-card'>"+makeName(name)+makeBlurb(blurb)+"</li>"
+}
+
+
+function makeName(name){
+  var htmlName = "<h1>"+name+"</h1>";
+  return htmlName;
+}
+
+function makeBlurb(blurb){
+  var htmlBlurb = "<p>"+blurb+"</p>";
+  return htmlBlurb;
+}
+
+
+  
